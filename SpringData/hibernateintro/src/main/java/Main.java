d7e821ddd388c7a9ec849d7d31ad98eb61d6ca59
Nikoleta <nikoletasamardzhieva@gmail.com>
2022-07-27 15:27:56 +0300

    Changes made

--- SpringData/hibernateintro/src/main/java/Main.java
+++ SpringData/hibernateintro/src/main/java/Main.java
@@ -1,14 +1,6 @@
-import org.hibernate.criterion.CriteriaQuery;
-import org.hibernate.query.QueryProducer;
-
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
-import javax.persistence.Query;
-import javax.persistence.criteria.CriteriaBuilder;
-import java.util.List;
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("school");
--- SpringData/hibernateintro/src/main/java/Student.java
+++ SpringData/hibernateintro/src/main/java/Student.java
@@ -30,31 +30,15 @@ public class Student {
         return id;
     }
 
-    public void setId(int id) {
-        this.id = id;
-    }
-
     public String getName() {
         return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
     }
 
     public String getSchoolName() {
         return schoolName;
     }
 
-    public void setSchoolName(String schoolName) {
-        this.schoolName = schoolName;
-    }
-
     public int getAge() {
         return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
     }
 }
